10e4dda772eb7e69f3f7f07628456a62f873b64d
yeyw <yeyw@moredian.com>
2018-04-11 08:55:37 +0800

    资源版本修改

--- sw.js
+++ sw.js
@@ -1,77 +1,77 @@
-  (function () {
-    var CACHE_VERSION = '2017121202';
+  (function() {
+      var CACHE_VERSION = '20100411';
 
-    var CONFIG = {
-      cacheName: 'notesCache' + CACHE_VERSION,
-      path: 'https://evanye1990.github.io/notes'
-    }
+      var CONFIG = {
+          cacheName: 'notesCache' + CACHE_VERSION,
+          path: 'https://evanye1990.github.io/notes'
+      }
 
-    /* 缓存列表 */
-    var filesToCache = [
-      CONFIG.path + '/',
-      CONFIG.path + '/assets/css/style.css',
-      CONFIG.path + '/assets/js/index.js'
-    ];
+      /* 缓存列表 */
+      var filesToCache = [
+          CONFIG.path + '/',
+          CONFIG.path + '/assets/css/style.css',
+          CONFIG.path + '/assets/js/index.js'
+      ];
 
-    self.addEventListener('install', function (e) {
-      console.log('[ServiceWorker] Install');
-      // e是一个InstallEvent对象,继承自ExtendableEvent
-      // 其中的waitUntil()方法接收一个promise对象，直到这个promise对象成功resolve之后，才会继续运行sw.js
-      e.waitUntil(
-        // caches是一个CacheStorage对象，使用open()方法打开一个缓存，缓存通过名称进行区分
-        // 获得cache实例之后，调用addAll()方法缓存文件
-        caches.open(CONFIG.cacheName).then(function (cache) {
-            console.log('[ServiceWorker] Caching app shell');
-            cache.addAll(filesToCache)
-            return false;
-          })
-      );
-    });
+      self.addEventListener('install', function(e) {
+          console.log('[ServiceWorker] Install');
+          // e是一个InstallEvent对象,继承自ExtendableEvent
+          // 其中的waitUntil()方法接收一个promise对象，直到这个promise对象成功resolve之后，才会继续运行sw.js
+          e.waitUntil(
+              // caches是一个CacheStorage对象，使用open()方法打开一个缓存，缓存通过名称进行区分
+              // 获得cache实例之后，调用addAll()方法缓存文件
+              caches.open(CONFIG.cacheName).then(function(cache) {
+                  console.log('[ServiceWorker] Caching app shell');
+                  cache.addAll(filesToCache)
+                  return false;
+              })
+          );
+      });
 
-    self.addEventListener('activate', function (e) {
-      console.log('[ServiceWorker] Activate');
-      e.waitUntil(
-        caches.keys().then(function (cacheNames) {
-          return Promise.all(cacheNames.map(function (cacheName) {
-            if (cacheName !== CONFIG.cacheName) {
-              console.log('[ServiceWorker] Removing old cache', cacheName);
-              return caches.delete(cacheName);
-            }
-          }))
-        })
-      );
-      return self.clients.claim();
-    });
+      self.addEventListener('activate', function(e) {
+          console.log('[ServiceWorker] Activate');
+          e.waitUntil(
+              caches.keys().then(function(cacheNames) {
+                  return Promise.all(cacheNames.map(function(cacheName) {
+                      if (cacheName !== CONFIG.cacheName) {
+                          console.log('[ServiceWorker] Removing old cache', cacheName);
+                          return caches.delete(cacheName);
+                      }
+                  }))
+              })
+          );
+          return self.clients.claim();
+      });
 
-    self.addEventListener('fetch', function (e) {
-      console.log('[Service Worker] Fetch', e.request.url);
-      /*
-       * The app is asking for app shell files. In this scenario the app uses the
-       * "Cache, falling back to the network" offline strategy:
-       * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
-       */
-      e.respondWith(
-        caches.match(e.request).then(function(response) {
-          if (response) {
-            return response;
-          }
+      self.addEventListener('fetch', function(e) {
+          console.log('[Service Worker] Fetch', e.request.url);
+          /*
+           * The app is asking for app shell files. In this scenario the app uses the
+           * "Cache, falling back to the network" offline strategy:
+           * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+           */
+          e.respondWith(
+              caches.match(e.request).then(function(response) {
+                  if (response) {
+                      return response;
+                  }
 
-          var fetchRequest = e.request.clone();
-          return fetch(fetchRequest).then(function (response) {
-            // 保证response的类型是basic，这表示请求本身是同源的，非同源（即跨域）的请求也不能被缓存
-            if (!response || response.status !== 200 || response.type !== 'basic') {
-              return response;
-            }
+                  var fetchRequest = e.request.clone();
+                  return fetch(fetchRequest).then(function(response) {
+                      // 保证response的类型是basic，这表示请求本身是同源的，非同源（即跨域）的请求也不能被缓存
+                      if (!response || response.status !== 200 || response.type !== 'basic') {
+                          return response;
+                      }
 
-            var responseToCache = response.clone();
-            caches.open(CONFIG.cacheName).then(function (cache) {
-              cache.put(e.request, responseToCache);
-            });
-            return response;
-          });
-        })
-      );
-    });
+                      var responseToCache = response.clone();
+                      caches.open(CONFIG.cacheName).then(function(cache) {
+                          cache.put(e.request, responseToCache);
+                      });
+                      return response;
+                  });
+              })
+          );
+      });
 
   })();
 
